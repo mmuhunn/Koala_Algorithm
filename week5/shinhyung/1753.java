9839797507c0f9c4e1b320b3cb86664596e8a0ea
ShinHyung Park <161359299+shinh09@users.noreply.github.com>
2025-05-16 13:20:21 +0900

    Create 1753.java

---
+++ week5/shinhyung/1753.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static class Edge implements Comparable<Edge> {
+        int to, cost;
+        public Edge(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine()); 
+
+        List<Edge>[] graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph[u].add(new Edge(v, cost));
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+
+            if (dist[cur.to] < cur.cost) continue;
+
+            for (Edge next : graph[cur.to]) {
+                if (dist[next.to] > dist[cur.to] + next.cost) {
+                    dist[next.to] = dist[cur.to] + next.cost;
+                    pq.offer(new Edge(next.to, dist[next.to]));
+                }
+            }
+        }
+        System.out.println(dist[end]);
+    }
+}
